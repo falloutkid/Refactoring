58cdf192a7aa8412396cbc70d127a10fa4f492d8
falloutkid <beck-13@mail.goo.ne.jp>
2013-10-26 03:18:31 +0900

    First Commit

---
+++ RentalMovie_2013/RentalMovie/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Movie
+    {
+        public const int CHILDRENS = 2;
+        public const int REGULAR = 0;
+        public const int NEW_RELEASE = 1;
+
+        private String movie_title;
+        private int price_code;
+
+        public Movie(String title, int priceCode)
+        {
+            movie_title = title;
+            price_code = priceCode;
+        }
+
+        public int PriceCode
+        {
+            get { return price_code; }
+            set { price_code = value; }
+        }
+
+        public string Title
+        {
+            get { return movie_title; }
+        }
+    }
+
+    public class Rental
+    {
+        private Movie rental_movie;
+        private int days_rented;
+
+        public Rental(Movie movie, int daysRented)
+        {
+            rental_movie = movie;
+            days_rented = daysRented;
+        }
+
+        public int DaysRented
+        {
+            get { return days_rented; }
+        }
+
+        public Movie Movie
+        {
+            get { return rental_movie; }
+        }
+    }
+
+    public class Customer
+    {
+        private string name;
+        private System.Collections.ArrayList rentals;
+
+        public Customer(string set_name)
+        {
+            rentals = new System.Collections.ArrayList( );
+            name = set_name;
+        }
+
+        public void addRental(Rental arg)
+        {
+            rentals.Add(arg);
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public double AmoutFor(Rental each)
+        {
+            return amountFor(each);
+        }
+
+        private double amountFor(Rental each)
+        {
+            double thisAmount = 0;
+            // 一行ごとに金額を計算
+            switch (each.Movie.PriceCode)
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.DaysRented > 2)
+                        thisAmount += (each.DaysRented - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.DaysRented * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.DaysRented > 3)
+                        thisAmount += (each.DaysRented - 3) * 1.5;
+                    break;
+            }
+            return thisAmount;
+        }
+
+        public string statement()
+        {
+            double totalAmount = 0;
+            int frequentRenterPoints = 0;
+            string result = "Rental Record for " + Name + "\n";
+            foreach (Rental each in rentals)
+            {
+                double thisAmount = 0;
+                // 一行ごとに金額を計算
+                switch (each.Movie.PriceCode)
+                {
+                    case Movie.REGULAR:
+                        thisAmount += 2;
+                        if (each.DaysRented > 2)
+                            thisAmount += (each.DaysRented - 2) * 1.5;
+                        break;
+                    case Movie.NEW_RELEASE:
+                        thisAmount += each.DaysRented * 3;
+                        break;
+                    case Movie.CHILDRENS:
+                        thisAmount += 1.5;
+                        if (each.DaysRented > 3)
+                            thisAmount += (each.DaysRented - 3) * 1.5;
+                        break;
+                }
+                // レンタルポイントを加算
+                frequentRenterPoints++;
+                // 新作を二日以上借りた場合はボーナスポイント
+                if ((each.Movie.PriceCode == Movie.NEW_RELEASE) &&
+                      each.DaysRented > 1)
+                    frequentRenterPoints++;
+                // この貸し出しに対する数値の表示
+                result += "\t" + each.Movie.Title + "\t" + thisAmount.ToString( ) + "\n";
+                totalAmount += thisAmount;
+            }
+            // フッタ部分の追加
+            result += "Amount owed is " + totalAmount.ToString( ) + "\n";
+            result += "You earned " + frequentRenterPoints.ToString( ) + " frequent renter points";
+            return result;
+        }
+    }
+}
---
+++ RentalMovie_2013/UnitTestProjectRentalMovie/TestRentalMovie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RentalMovie;
+
+namespace TestRentalMovie
+{
+    [TestFixture()]
+    public class TestClass
+    {
+
+        [Test()]
+        public void Teststatement()
+        {
+            string name = "Taro";
+            string title = "star wars";
+            int days = 5;
+            int thisAmount = 15;
+            int frequentRenterPoints = 2;
+            Customer customer = new Customer(name);
+            Movie sample_movie = new Movie(title, Movie.NEW_RELEASE);
+            Rental rental = new Rental(sample_movie, days);
+            customer.addRental(rental);
+            string result = customer.statement();
+
+
+            string expect = "Rental Record for " + name + "\n";
+            expect += "\t" + sample_movie.Title + "\t" + thisAmount.ToString() + "\n";
+            expect += "Amount owed is 15" + "\nYou earned " + frequentRenterPoints.ToString() + " frequent renter points";
+            Assert.AreEqual(expect, result, "Wrong!! expect[{0}] result[{1}]", expect, result);
+        }
+
+        [TestCase(1, 1, 1.0)]
+        public void TestamountFor(int days, int price_code, double expect)
+        {
+            Movie movie_input = new Movie("sample", price_code);
+            Rental rental_input = new Rental(movie_input, days);
+
+        }
+    }
+}
